afcfd1128a1fef85ca7a328fe9cbc9ba984fe490
at4 <at4@jc4.so-net.ne.jp>
2026-05-18 19:06:15 +0200

    Update Dots

--- src/Components/Dots.tsx
+++ src/Components/Dots.tsx
@@ -1,14 +1,13 @@
-import { useWindowSize } from "@uidotdev/usehooks";
+import { isMobile } from "react-device-detect";
 import Styles from "@/styles/Dots.module.scss";
 
 export const Dots = () => {
-	const size = useWindowSize();
-	const windowWidth = size.width ?? 640;
-	const windowHeight = size.height ?? 480;
+	const canvasWidth = isMobile ? 4000 : 10000;
+	const canvasHeight = isMobile ? 4000 : 10000;
 
 	return (
 		<div className={Styles.container}>
-			<canvas id={"dots"} width={windowWidth} height={windowHeight} />
+			<canvas id={"dots"} width={canvasWidth} height={canvasHeight} />
 		</div>
 	);
 };
