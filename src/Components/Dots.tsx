3ace34c0d41a647e8aebffabe4d36ca154be1a44
at4 <at4@jc4.so-net.ne.jp>
2026-05-22 10:53:01 +0200

    Update and fix functions

--- src/Components/Dots.tsx
+++ src/Components/Dots.tsx
@@ -1,10 +1,8 @@
-import { useCallback, useEffect, useState, useMemo, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { isMobile } from "react-device-detect";
-import { agrippa } from "@/vo/Agrippa";
-import { ariadne } from "@/vo/Ariadne";
-import { laocoon } from "@/vo/Laocoon";
 import { getEasedCoordinates } from "@/functions/getEasedCoordinates";
 import Styles from "@/styles/Dots.module.scss";
+import { useDots } from "@/hooks/useDots";
 
 export const Dots = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -14,112 +12,13 @@ export const Dots = () => {
 	const breakFlag = useRef(false);
 	const requestRef = useRef<number>(undefined);
 
-	const getMax = useCallback((isOutside: boolean = false) => {
-		const max = Math.max(window.innerWidth, window.innerHeight);
-		let x: number;
-		let y: number;
-
-		if (isOutside) {
-			const tmpX = Math.random() * 40 - 20;
-			const tmpY = Math.random() * 40 - 20;
-
-			if (Math.floor(Math.random() * 2) === 1) {
-				x = tmpX < 0 ? tmpX : window.innerWidth + tmpX;
-				y = Math.random() * (max - 40) - 20;
-			} else {
-				x = Math.random() * (max - 40) - 20;
-				y = tmpY < 0 ? tmpY : window.innerHeight + tmpY;
-			}
-		} else {
-			x = Math.random() * (max - 40) - 20;
-			y = Math.random() * (max - 40) - 20;
-		}
-
-		return { x, y };
-	}, []);
-
-	const dotsNumber = useRef(0);
-	const dotsArray = useMemo(() => [agrippa, ariadne, laocoon], []);
-	const [dots, setDots] = useState(dotsArray[0]);
-
-	const setDotsArray = useCallback(
-		(isInit: boolean = false) => {
-			const tmpDots = dots;
-
-			if (!isInit) {
-				dotsNumber.current++;
-				if (dotsNumber.current > dotsArray.length - 1) {
-					dotsNumber.current = 0;
-				}
-			}
-
-			const num = dotsNumber.current;
-
-			for (let i = 0; i < dotsArray[num].length; i++) {
-				let currentX = 0;
-				let currentY = 0;
-
-				if (isInit) {
-					const { x, y } = getMax(false);
-					currentX = x;
-					currentY = y;
-				} else if (!tmpDots[i]) {
-					const { x, y } = getMax(true);
-					currentX = x;
-					currentY = y;
-				} else {
-					currentX = tmpDots[i][6];
-					currentY = tmpDots[i][7];
-				}
-
-				const tmpArray = [];
-				const startX = dotsArray[num][i][0];
-				const startY = dotsArray[num][i][1];
-
-				// x, y, startX, startY, endX, endY, currentX, currentY, duration, currentDuration
-				tmpArray[0] = startX;
-				tmpArray[1] = startY;
-
-				tmpArray[2] = currentX;
-				tmpArray[3] = currentY;
-
-				tmpArray[4] = startX + Math.floor((window.innerWidth - 300) / 2);
-				tmpArray[5] = startY + Math.floor((window.innerHeight - 340) / 2);
-
-				tmpArray[6] = currentX;
-				tmpArray[7] = currentY;
-
-				tmpArray[8] = Math.floor(Math.random() * 220 + 20);
-				tmpArray[9] = 0;
-
-				tmpDots[i] = tmpArray;
-			}
-
-			setDots(tmpDots);
-		},
-		[getMax, dotsArray, dots],
-	);
+	const { setDotsArray, setBreakDotsArray, updateDotsArray, getDotsArray } =
+		useDots();
 
 	const breakInit = useCallback(() => {
-		for (let i = 0; i < dots.length; i++) {
-			const { x, y } = getMax(false);
-
-			// x, y, startX, startY, endX, endY, currentX, currentY, duration, currentDuration, num
-			dots[i][2] = dots[i][6];
-			dots[i][3] = dots[i][7];
-
-			dots[i][4] = x;
-			dots[i][5] = y;
-
-			// dots[i][6] = dots[i][6];
-			// dots[i][7] = dots[i][7];
-
-			dots[i][8] = 24;
-			dots[i][9] = 0;
-		}
-
+		setBreakDotsArray();
 		breakFlag.current = true;
-	}, [getMax, dots]);
+	}, [setBreakDotsArray]);
 
 	const animate = useCallback(() => {
 		if (!canvasRef.current) {
@@ -138,6 +37,8 @@ export const Dots = () => {
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 		ctx.fillStyle = "#000";
 
+		const dots = getDotsArray();
+
 		for (let i = 0; i < dots.length; i++) {
 			const { currentX, currentY } = getEasedCoordinates({
 				startX: dots[i][2],
@@ -151,10 +52,7 @@ export const Dots = () => {
 
 			ctx.fillRect(currentX, currentY, 1, 1);
 
-			dots[i][6] = currentX;
-			dots[i][7] = currentY;
-
-			dots[i][9]++;
+			updateDotsArray(i, currentX, currentY);
 		}
 
 		// Move designated positions after break
@@ -167,7 +65,7 @@ export const Dots = () => {
 		}
 
 		requestRef.current = requestAnimationFrame(animate);
-	}, [canvasWidth, canvasHeight, setDotsArray, dots]);
+	}, [canvasWidth, canvasHeight, setDotsArray, updateDotsArray, getDotsArray]);
 
 	const click = useCallback(() => {
 		if (requestRef.current) {
---
+++ src/hooks/useDots.ts
@@ -0,0 +1,148 @@
+import { useCallback, useMemo, useRef } from "react";
+import { agrippa } from "@/vo/Agrippa";
+import { ariadne } from "@/vo/Ariadne";
+import { laocoon } from "@/vo/Laocoon";
+
+export const useDots = () => {
+	const dots = useRef<number[][]>([]);
+
+	const dotsNumber = useRef(0);
+	const dotsArray = useMemo(() => [agrippa, ariadne, laocoon], []);
+
+	const getMax = useCallback((isOutside: boolean = false) => {
+		const max = Math.max(window.innerWidth, window.innerHeight);
+		let x: number;
+		let y: number;
+
+		if (isOutside) {
+			const tmpX = Math.random() * 40 - 20;
+			const tmpY = Math.random() * 40 - 20;
+
+			if (Math.floor(Math.random() * 2) === 1) {
+				x = tmpX < 0 ? tmpX : window.innerWidth + tmpX;
+				y = Math.random() * (max - 40) - 20;
+			} else {
+				x = Math.random() * (max - 40) - 20;
+				y = tmpY < 0 ? tmpY : window.innerHeight + tmpY;
+			}
+		} else {
+			x = Math.random() * (max - 40) - 20;
+			y = Math.random() * (max - 40) - 20;
+		}
+
+		return { x, y };
+	}, []);
+
+	const setDotsArray = useCallback(
+		(isInit = false) => {
+			if (!isInit) {
+				dotsNumber.current++;
+				if (dotsNumber.current > dotsArray.length - 1) {
+					dotsNumber.current = 0;
+				}
+			}
+
+			const oldDots = dots.current;
+			const tmpDots = dotsArray[dotsNumber.current];
+
+			for (let i = 0; i < tmpDots.length; i++) {
+				let currentX: number;
+				let currentY: number;
+
+				if (isInit) {
+					const { x, y } = getMax(false);
+					currentX = x;
+					currentY = y;
+				} else if (!oldDots[i]) {
+					const { x, y } = getMax(true);
+					currentX = x;
+					currentY = y;
+				} else {
+					currentX = oldDots[i][6];
+					currentY = oldDots[i][7];
+				}
+
+				const tmpLine = [];
+				const startX = tmpDots[i][0];
+				const startY = tmpDots[i][1];
+
+				// x, y, startX, startY, endX, endY, currentX, currentY, duration, currentDuration
+				tmpLine.push(
+					startX,
+					startY,
+
+					currentX,
+					currentY,
+
+					startX + Math.floor((window.innerWidth - 300) / 2),
+					startY + Math.floor((window.innerHeight - 340) / 2),
+
+					currentX,
+					currentY,
+
+					Math.floor(Math.random() * 220 + 20),
+					0,
+				);
+
+				tmpDots[i] = tmpLine;
+			}
+
+			dots.current = tmpDots;
+		},
+		[dotsArray, getMax],
+	);
+
+	const setBreakDotsArray = useCallback(
+		(t: number = 24) => {
+			const tmpDots = dots.current;
+
+			for (let i = 0; i < tmpDots.length; i++) {
+				const { x, y } = getMax(false);
+
+				const tmpLine = tmpDots[i];
+				const startX = tmpDots[i][6];
+				const startY = tmpDots[i][7];
+
+				// x, y, startX, startY, endX, endY, currentX, currentY, duration, currentDuration, num
+				tmpLine[2] = startX;
+				tmpLine[3] = startY;
+
+				tmpLine[4] = x;
+				tmpLine[5] = y;
+
+				tmpLine[8] = t;
+				tmpLine[9] = 0;
+
+				tmpDots[i] = tmpLine;
+			}
+
+			dots.current = tmpDots;
+		},
+		[getMax],
+	);
+
+	const updateDotsArray = useCallback(
+		(i: number, currentX: number, currentY: number) => {
+			const tmpDots = dots.current;
+			const tmpLine = tmpDots[i];
+			tmpLine[6] = currentX;
+			tmpLine[7] = currentY;
+			tmpLine[9] += 1;
+
+			tmpDots[i] = tmpLine;
+			dots.current = tmpDots;
+		},
+		[],
+	);
+
+	const getDotsArray = useCallback(() => {
+		return dots.current;
+	}, []);
+
+	return {
+		setDotsArray,
+		setBreakDotsArray,
+		updateDotsArray,
+		getDotsArray,
+	};
+};
