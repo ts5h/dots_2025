870198f26812c5b95ff9b01113d8ea2b29e257eb
at4 <at4@jc4.so-net.ne.jp>
2026-05-25 11:48:44 +0200

    Update and fix navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Dots } from "@/Components/Dots";
 import "@/styles/App.css";
 import { MenuHome } from "@/Components/Menu/Home";
+import { Menu } from "@/Components/Menu";
 
 function App() {
 	return (
 		<>
 			<Dots />
 			<MenuHome />
+			<Menu repositoryUrl={"https://github.com/ts5h/dots_2025"} />
 		</>
 	);
 }
